f9a8f1ebaeeacbc1ac42fe0fb9d7fdd63ddbe934
Brunoss02 <box.brunossantos@gmail.com>
2022-07-25 21:41:00 -0300

    Alterando O download

--- Projetos_dir/Projeto005.py
+++ Projetos_dir/Projeto005.py
@@ -9,6 +9,7 @@ Passo 6: Mandar um email para a diretoria com os indicadores.
 import pyautogui
 import pyperclip
 from time import sleep
+import pandas
 
 pyautogui.PAUSE = 2
 
@@ -31,5 +32,10 @@ sleep(3)
 
 # Passo 2: Navegar no sistema e encontrar a base de dados (entrar na pasta Exportar)
 pyautogui.click(x=159, y=287, clicks=2)
-pyautogui.click(x=922, y=183)
+
+# Passo 3: Download da base de dados.
+# pyautogui.click(x=922, y=183)
+
+# Passo 4: Calcular os indicadores (faturamento, quantidade de produtos)
+tabela = pandas.read_excel(r"C:\Users\FamilySantos\Desktop\Projeto Python\Aula 1-20220725T231158Z-001\Aula 1\Exportar\Vendas - Dez.xlsx")
 # print(pyautogui.position())
