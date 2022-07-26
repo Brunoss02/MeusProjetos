cfa1d0d040b36fff940a4c5600be9f3cfb124686
Brunoss02 <box.brunossantos@gmail.com>
2022-07-25 22:06:54 -0300

    Atualizando POO para Pessoa

--- Projetos_dir/POOarquivo.py
+++ Projetos_dir/POOarquivo.py
@@ -1,1 +1,11 @@
-print("Ola Mundo")
+class Pessoa:
+    from datetime import date
+
+    ano_atual = date.today().year
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def ano_nascimento(self):
+        return self.ano_atual - self.idade
